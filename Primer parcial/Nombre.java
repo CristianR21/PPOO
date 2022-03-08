d408b89ad89bf4874a0b064ef019de7ea60d596a
CristianR21 <101162807+CristianR21@users.noreply.github.com>
2022-03-08 12:48:55 -0600

    tarea nombre(casa)
    07/03/2022

---
+++ Primer parcial/Nombre.java
@@ -0,0 +1,46 @@
+import java.util.*;
+//cristian adair ramirez rodriguez
+//fecha:7 de marzo de 2022
+public class Main {
+    public static void main(String[] args) throws Exception {
+System.out.println("		          #	");
+System.out.println("		         ##	");
+System.out.println("		        ####	");
+System.out.println("		      ########	");
+System.out.println("             # *********** #");
+System.out.println("            #  ***********  #");
+System.out.println("           #   **		   #");
+System.out.println("          #    **		    #");
+System.out.println("         #     **		     #");
+System.out.println("        #      ***********      #");
+System.out.println("       #       ***********       #");
+System.out.println("");
+System.out.println("       #       ***********        #");
+System.out.println("       #       **       **        #");
+System.out.println("       #       **       **        #");
+System.out.println("       #       ***********        #");
+System.out.println("       #       *        **        #");
+System.out.println("       #       *         *        #");
+System.out.println("");
+System.out.println("       #       ***********        #");
+System.out.println("       #       ***********        #");
+System.out.println("       #           **             #");
+System.out.println("       #           **             #");
+System.out.println("       #           **             #");
+System.out.println("       #       ***********        #");
+System.out.println("");
+System.out.println("       #       ***********        #");
+System.out.println("       #       **                 #");
+System.out.println("       #       **                 #");
+System.out.println("       #       ***********        #");
+System.out.println("       #                 *        #");
+System.out.println("       #       ***********        #");
+System.out.println("");
+System.out.println("       # # # # ## # # # # # # # # #");
+System.out.println("       #                        #");
+System.out.println("        #                      #");
+System.out.println("         #                    #");
+System.out.println("          #   (**********)   #");
+System.out.println("           #  (**********)  #");
+    }
+}
