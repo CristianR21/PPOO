30de3969831cf8782cf8345f27bd08b104b2d429
CristianR21 <101162807+CristianR21@users.noreply.github.com>
2022-03-15 12:12:21 -0600

    actualizacion
    principal

--- programas/eclipse-workspace/Figura/src/principal/Principal.java
+++ programas/eclipse-workspace/Figura/src/principal/Principal.java
@@ -3,11 +3,16 @@ package principal;
 public class Principal {
 	public static void main(String[] args) {
 		//primitivos
-		int num;
-		num=20;
-		float flotante;
-		flotante=5.7+60;
+		int num=20;
+		float flotante=10;
+		long largo;
+		boolean verd;
+		char numero=123;
+		byte num2=3;
+		short num3;
+		double numB=212.1
 		System.out.println("el numero es:" + num);
+		System.out.println("el numero es:" + numero);
 		System.out.println("el numero es:" + flotante);
 	}
 
